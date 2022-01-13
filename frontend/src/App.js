2610d20f6b536ac9f48425f23a34aa85dcade221
Alish Palasara <palasaraalis@gmail.com>
2022-01-13 07:27:28 +0530

    Header and Footer Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+
 const App = () => {
   return (
-    <div className="App">
-      <h2>Welcome to EL cart</h2>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h2>Welcome to EL cart</h2>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 };
 
